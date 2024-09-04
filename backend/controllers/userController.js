fcad819e00b97093a2a69a9bf75af308a5b19864
Junaid Syed <sjunaid626@gmail.com>
2024-09-04 21:34:47 +0530

    chore: Add user controller with find all users and find users by home ID functions

---
+++ backend/controllers/userController.js
@@ -0,0 +1,31 @@
+const User = require("../models/userModel");
+const Home = require("../models/homeModel");
+// const UserHomeRelation = require('../models/userHomeRelationModel');
+
+// Find all users
+exports.findAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Find users by home ID
+exports.findUsersByHome = async (req, res) => {
+  try {
+    const homeId = req.params.homeId;
+    const home = await Home.findByPk(homeId, {
+      include: User,
+    });
+
+    if (!home) {
+      return res.status(404).json({ message: "Home not found" });
+    }
+
+    res.status(200).json(home.users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
