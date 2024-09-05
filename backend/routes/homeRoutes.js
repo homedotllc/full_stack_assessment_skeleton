b4293b4f725aec4f11894b143bbd61158809722d
Junaid Syed <sjunaid626@gmail.com>
2024-09-05 15:04:17 +0530

    refactor: Update homeRoutes.js to include pagination for fetching homes by user

--- backend/routes/homeRoutes.js
+++ backend/routes/homeRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const { getHomesByUser, updateUsersForHome, getAllUsersByHome } = require('../controllers/homeController');
+const { updateUsersForHome, getAllUsersByHome, getPaginatedHomesByUser } = require('../controllers/homeController');
 
 const router = express.Router();
 
-// Route to get all homes associated with a specific user
-router.get('/find-by-user/:userId', getHomesByUser);
 
 // Route to update users associated with a specific home
 router.put('/update-users/:homeId', updateUsersForHome);
 
 // Route to fetch all users and mark those associated with a specific home
 router.get('/get-users-by-home/:homeId', getAllUsersByHome);
+
+// Get paginated homes for a user
+router.get('/find-by-user/:userId', getPaginatedHomesByUser);
 
 module.exports = router;
