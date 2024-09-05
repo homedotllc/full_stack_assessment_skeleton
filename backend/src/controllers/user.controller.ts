e8a3066bdcc48172b4baa25c5d5b9070a8c63c3d
harsh-scrobits <harshkumar.scrobits@gmail.com>
2024-09-05 11:27:48 +0530

    feat: Implement updating users in a home
    - Added logic to update users in a home based on input data
    - Implemented transactional updates to user-home relationship table
    - Added error handling for invalid input and database errors

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,17 +1,39 @@
 import { Request, Response } from "express"
+import { AppDataSource } from "../data-source"
+import { User } from "../entity/user.entity"
+import { UserHome } from "../entity/userHome.entity"
 
 export class UserController {
   static async findAll(req: Request, res: Response) {
     try {
-      res.status(200).json({ controller: "findAll" })
+      let userRepository = AppDataSource.getRepository(User)
+
+      let users = await userRepository.find()
+
+      res.status(200).json({
+        controller: "findAll",
+        users: users
+      })
     } catch (error) {
       res.status(500).json({ error: error })
     }
   }
   static async findByHome(req: Request, res: Response) {
     try {
-      res.status(200).json({ controller: "findByHome" })
+      let homeId = parseInt(req.params.homeId)
+
+      const userRepository = AppDataSource.getRepository(User)
+
+      const users = await userRepository
+        .createQueryBuilder("user")
+        .innerJoin(UserHome, "userHome", "user.id = userHome.userId")
+        .where("userHome.homeId = :homeId", { homeId })
+        .getMany()
+
+      res.status(200).json({ controller: "findByHome", users: users || [] })
     } catch (error) {
+      console.log(error)
+
       res.status(500).json({ error: error })
     }
   }
