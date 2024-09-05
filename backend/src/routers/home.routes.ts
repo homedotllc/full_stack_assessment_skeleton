dc00b4b21b86c3b434aa11be82f3ebfb39cbc9f5
harsh-scrobits <harshkumar.scrobits@gmail.com>
2024-09-05 11:38:33 +0530

    feat: Update find-by-user route to accept userId parameter

--- backend/src/routers/home.routes.ts
+++ backend/src/routers/home.routes.ts
@@ -3,4 +3,4 @@ import { HomeController } from "../controllers/home.controller"
 export const Router = express.Router()
 
 Router.post("/update-users", HomeController.updateUser)
-Router.get("/find-by-user", HomeController.findByUser)
+Router.get("/find-by-user/:userId", HomeController.findByUser)
