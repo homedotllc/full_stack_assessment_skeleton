25dc520437b9ecae16f6e96b148640b1f517b385
harsh-scrobits <harshkumar.scrobits@gmail.com>
2024-09-05 11:38:42 +0530

    feat: Update user.routes to include dynamic homeId parameter

--- backend/src/routers/user.routes.ts
+++ backend/src/routers/user.routes.ts
@@ -3,4 +3,4 @@ import { UserController } from "../controllers/user.controller"
 export const Router = express.Router()
 
 Router.get("/find-all", UserController.findAll)
-Router.get("/find-by-home", UserController.findByHome)
+Router.get("/find-by-home/:homeId", UserController.findByHome)
