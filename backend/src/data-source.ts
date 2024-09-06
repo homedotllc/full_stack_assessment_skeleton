6869291bebe8f7806df14d020a4350b1e0965f1c
coolharshkumar4 <coolharshkumar4@gmail.com>
2024-09-07 00:21:00 +0530

    chore(backend): update build and configuration files
    - Update .gitignore to exclude dist directory
    - Modify package.json scripts to use dist directory for compiled files
    - Update tsconfig.json to set outDir to dist
    - Update data-source.ts to use explicit entity imports
    
    Update project configuration and build settings to use the dist directory for compiled output files. This change ensures a cleaner separation between source and compiled code, aligning with best practices. The modifications include updating relevant scripts, TypeScript configuration, and data source setup to accommodate the new dist directory structure.

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,7 +1,9 @@
 import * as dotenv from "dotenv"
-import { join } from "path"
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import { Home } from "./entity/home.entity"
+import { User } from "./entity/user.entity"
+import { UserHome } from "./entity/userHome.entity"
 dotenv.config()
 
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME, NODE_ENV } = process.env
@@ -12,8 +14,9 @@ export const AppDataSource = new DataSource({
   username: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
-  entities: [join(__dirname, "**", "*.entity.{ts,js}")],
-  // entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+  entities: [User, Home, UserHome],
+  // entities: [join(__dirname, "**", "*.entity.{ts,js}")],
+  // entities: [__dirname + "/**/*.entity{.ts,.js}"],
   migrations: [],
   subscribers: [],
   // synchronize: NODE_ENV === "development" ? true : false,
