bb17a9a0ea760155c024d180de3bccd760821b59
harsh-scrobits <harshkumar.scrobits@gmail.com>
2024-09-06 12:49:51 +0530

    feat: Add async error handling to route functions.

--- backend/src/routers/home.routes.ts
+++ backend/src/routers/home.routes.ts
@@ -1,6 +1,10 @@
 import express from "express"
 import { HomeController } from "../controllers/home.controller"
+import { asyncErrorHandler } from "../utils/error.handler"
 export const Router = express.Router()
 
-Router.post("/update-users", HomeController.updateUser)
-Router.get("/find-by-user/:userId", HomeController.findByUser)
+Router.post("/update-users", asyncErrorHandler(HomeController.updateUser))
+Router.get(
+  "/find-by-user/:userId",
+  asyncErrorHandler(HomeController.findByUser)
+)
