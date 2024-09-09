6861e28d4e0a18764271c30c5bc33fe012612fdb
Aahan215 <56781825+Aahan215@users.noreply.github.com>
2024-09-09 07:16:11 +0530

    Update 99_final_db_dump.sql
    Updated SQL

--- sql/99_final_db_dump.sql
+++ sql/99_final_db_dump.sql
@@ -1,13 +1,6 @@
 use home_db;
 CREATE TABLE user as select distinct username, email from user_home;
 CREATE TABLE home as select distinct street_address, state, zip, sqft,beds,baths,list_price from user_home;
-CREATE TABLE user_home_mapping (
-    user_id INT,
-    home_id INT,
-    PRIMARY KEY (user_id, home_id),
-    FOREIGN KEY (user_id) REFERENCES user(id),
-    FOREIGN KEY (home_id) REFERENCES home(id)
-);
 
 ALTER TABLE home ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY;
 ALTER TABLE user ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY;
@@ -17,6 +10,15 @@ ALTER TABLE home ADD UNIQUE KEY (street_address);
 
 ALTER TABLE user MODIFY COLUMN username varchar(100) NOT NULL;
 ALTER TABLE home MODIFY COLUMN street_address varchar(255) NOT NULL;
+
+CREATE TABLE user_home_mapping (
+    user_id INT,
+    home_id INT,
+    PRIMARY KEY (user_id, home_id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (home_id) REFERENCES home(id)
+);
+
 
 INSERT INTO user_home_mapping (user_id, home_id) 
 	select u.id, h.id from user_home uh
